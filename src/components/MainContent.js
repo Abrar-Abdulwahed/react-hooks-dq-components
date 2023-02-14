1b3cd460de7cb9179317e852baf70a419074a609
A-Abd <abrar.abdulwahed@gmail.com>
2023-02-14 23:52:15 +0300

    Completed assignment with bonus section

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,13 @@
 import React from "react";
+import AlbumList from "./AlbumList";
+import GenrePicker from "./GenrePicker";
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>
+    {/* What components go here? */}
+    <GenrePicker />
+    <AlbumList />
+    </main>;
 }
 
 export default MainContent;
